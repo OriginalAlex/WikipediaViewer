70fa61f3f616a17acbf510a65df8bfbc7c8bc2e6
OriginalAlex <ajcartwright.alex288+1@gmail.com>
2017-11-24 21:27:40 +0000

    Reduced number of API calls per request

--- script.js
+++ script.js
@@ -79,30 +79,18 @@ function stripTags(str) {
 
 function loadMore(x, container) {
     var m = 0;
-    var query = "https://en.wikipedia.org/w/api.php?format=json&action=query&srlimit=50&prop=extracts&explaintext=1&exintro=1&titles=";
     for (var article in articles) {
         if (m++ <= articleNum) continue;
         if (m == x + articleNum+2) break;
         article = articles[article];
-        query += article.title.split(" ").join("_");
-        if (m != articles.length-1) query += "|";
         container.innerHTML +=
-        '<a target="_blank" class="link" href="https://en.wikipedia.org/?curid=' + article.pageid + '"><div class="article"><h3 class="title">' + article.title + '</h3><div class="summary" id="' + article.title + '"></div></div></a>';
+        '<a target="_blank" class="link" href="' + article.fullurl + '"><div class="article"><h3 class="title">' + article.title + '</h3><div class="summary">' + getFirstFullStop(article.extract, 2)  + '</div></div></a>';
     }
     if (m < x + articleNum + 2) { // the loop did not run fully ie. we ran out of articles
         var load = document.getElementById("loadMore");
         makeLoadButtonDisabled(load);
     }
-    asyncGet(query, function(reply) {
-        var pages = reply.query.pages;
-        for (var i in pages) {
-            try {
-                var page = (pages[i]);
-                document.getElementById(page.title).innerHTML = getFirstFullStop(page.extract, 0, 2);
-            } catch (e) {}
-        }
-        articleNum += x;
-    });
+    articleNum += x;
 }
 
 function fetchArticles(input) {
@@ -134,7 +122,7 @@ function handleClick() {
     remove("padder");
     makeLoadButtonEnabled(document.getElementById("loadMore"));
     var input = document.getElementById("search").value,
-        url = "https://en.wikipedia.org/w/api.php?action=query&srlimit=50&format=json&list=search&srsearch=" + input;
+        url = "https://en.wikipedia.org/w/api.php?action=query&srlimit=50&format=json&rvprop=content&rvsection=0&list=search&srsearch=" + input;
     fetchArticles(input);
 }
 
